25ab8c7a00e79d2a5cb8697e3cabef3948435470
thatvanzh <that.vanzh@gmail.com>
2026-03-02 13:14:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
-//node server which will handle socket io connections
-const io=require('socket.io')(8000, {
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
     cors: {
-        origin:"http://127.0.0.1:5500",
-        methods:["GET","POST"]
+        origin: "*",
+        methods: ["GET", "POST"]
     }
 });
-const user={};
-io.on('connection', socket=>{
-    socket.on('new-user-joined',name=>{
-        console.log(name)
-        console.log(" joined the chat")
-        user[socket.id]=name;
-        socket.broadcast.emit('user-joined',name);
+
+const users = {};
+
+// Serve frontend files
+app.use(express.static(__dirname));
+
+io.on("connection", socket => {
+
+    socket.on("new-user-joined", name => {
+        users[socket.id] = name;
+        socket.broadcast.emit("user-joined", name);
     });
-    socket.on('send',message=>{
-        socket.broadcast.emit('receive',{message: message,name: user[socket.id]})
+
+    socket.on("send", message => {
+        socket.broadcast.emit("receive", {
+            message: message,
+            name: users[socket.id]
+        });
     });
-    socket.on('disconnect',message=>{
-        socket.broadcast.emit('left',user[socket.id])
-        delete user[socket.id]
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("left", users[socket.id]);
+        delete users[socket.id];
     });
-})
+});
+
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
