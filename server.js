e56aa4171a89d4fb2004a7fec3a603c3d6152e84
thatvanzh <that.vanzh@gmail.com>
2026-03-02 13:50:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
+const express = require('express');
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
 
-app.use(express.static(__dirname));
+// Serve the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-io.on("connection", (socket) => {
-    console.log("User connected");
+// Socket.io Logic (The Chat Brain)
+io.on('connection', (socket) => {
+    
+    // When a new user joins
+    socket.on('newuser', (username) => {
+        socket.broadcast.emit('update', username + " has joined the conversation");
+    });
 
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
+    // When a user disconnects
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('update', "A user has left the conversation");
+    });
+
+    // When a user sends a message
+    socket.on('chat', (message) => {
+        socket.broadcast.emit('chat', message);
     });
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
+// Listen on the port Render assigns (or 3000 locally)
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
