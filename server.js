2b4fc6065df11fc0b059f63ebd8eed3e0195e705
thatvanzh <that.vanzh@gmail.com>
2026-03-02 12:54:45 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
